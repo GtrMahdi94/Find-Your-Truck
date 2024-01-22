1c4ea7c32ed38ff3aaa02bd779c86d4036267340
Gatri Mahdi <gatrimahdi5@gmail.com>
2024-01-22 16:09:57 +0100

    one

---
+++ index.js
@@ -0,0 +1,30 @@
+
+$(".insc").hide()
+$(".contact").hide()
+$(".info").hide()
+
+function test(){
+  $(".AcceilDiv").hide()
+  $(".contact").hide()
+  $(".info").hide()
+  $(".insc").show()
+}
+function test1(){
+  $(".AcceilDiv").hide()
+  $(".info").hide()
+  $(".insc").hide()
+  $(".contact").show()
+}
+function test2(){
+  $(".AcceilDiv").hide()
+  $(".insc").hide()
+  $(".contact").hide()
+  $(".info").show()
+}
+function test3(){
+  $(".insc").hide()
+  $(".contact").hide()
+  $(".info").hide()
+  $(".AcceilDiv").show()
+ 
+}
